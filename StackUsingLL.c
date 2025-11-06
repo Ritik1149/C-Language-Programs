8fdeb78868b7b28f91af0dafe2b53618ac21ec68
Ritik Asija <ritikasija200409@gmail.com>
2025-11-06 08:23:08 +0530

    Refactored stack implementation using linked list

--- StackUsingLL.c
+++ StackUsingLL.c
@@ -1,58 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct nodeDesign
+{
+    int data;
+    struct nodeDesign *prev,*next;
+};
+
+struct nodeDesign *head=NULL,*node,*ptr;
+int i;
+
+void push(int size, int *top)
+{
+    if(*top==size-1)
+    {
+        printf("\nOverflow");
+    }
+    else
+    {
+        node=(struct nodeDesign*)malloc(sizeof(struct nodeDesign));
+        if(*top==-1)
+        {
+            node->next=head;
+            node->prev=head;
+            head=node;
+        }
+        else
+        {
+            ptr=head;
+            for(i=0;i<(*top);i++)
+            {
+                ptr=ptr->next;
+            }
+            node->next=ptr->next;
+            node->prev=ptr;
+            ptr->next=node;
+        }
+        *top=*top+1;
+        printf("\nEnter the element: ");
+        scanf("%d",&(node->data));
+        printf("\n%d pushed successfully", node->data);
+    }
+}
+
+void pop(int *top)
+{
+    if(*top==-1)
+    {
+        printf("\nUnderflow");
+    }
+    else
+    {
+        ptr=head;
+        for(i=0;i<(*top);i++)
+        {
+            ptr=ptr->next;
+        }
+        printf("\n%d Deleted",ptr->data);
+        (ptr->prev)->next=ptr->next;
+        free(ptr);
+        *top=*top-1;
+    }
+}
+
+void peek(int top)
+{
+    if(top==-1)
+    {
+        printf("Stack is empty.");
+    }
+    else
+    {
+        ptr=head;
+        for(i=0;i<top;i++)
+        {
+            ptr=ptr->next;
+        }
+        printf("\nTop Element = %d", ptr->data);
+    }
+}
+
+void display(int top)
+{
+    printf("[");
+    ptr=head;
+    for(i=0;i<top;i++)
+    {
+        ptr=ptr->next;
+    }
+    for(i=top;i>=0;i--)
+    {
+        printf("\n%d",ptr->data);
+        ptr=ptr->prev;
+    }
+    printf("\n]");
+}
+
 int main()
 {
-    struct nodeDesign{
-        int data;
-        struct nodeDesign *next;
-    };
+    int size,choice,top=-1;
+    
+    printf("Enter the size of stack: ");
+    scanf("%d",&size);
 
-    struct nodeDesign *head,*node, *ptr;
-
-    head=node;
-    int choice,choiceIns,element,pos;
-
-    printf("Menu\n\t1.Insert\n\t2.Delete\n\t3.Print\n\t4.Exit");
+    printf("\nStack Operations:\n\t1.Push\n\t2.Pop\n\t3.Peek\n\t4.Display\n\t5.Check if Full\n\t6.Check if Empty\n\t7.Exit");
+    
     while(1)
     {
-        printf("\n\nEnter your choice: ");
+        printf("\nEnter your choice: ");
         scanf("%d",&choice);
 
         switch(choice)
         {
             case 1:
-                node=(struct nodeDesign*)malloc(sizeof(struct nodeDesign));
-                if(node==NULL)
-                {
-                    printf("\nMemory Allocation Failure...");
-                    getch();
-                    exit(1);
-                }
-                printf("\nMenu\n\t1.Insert at beginning\n\t2.Insert at end\n\t3.Insert at a location\n\nEnter your choice: ");
-                scanf("%d",&choiceIns);
-                switch(choiceIns)
-                {
-                    case 1:
-                        node->next=head;
-                        head=node;
-                        printf("\nEnter the element to insert: ");
-                        scanf("%d",&(node->data));
-                        printf("%d inserted successfully!", node->data);
-                    case 2:
-                        ptr=head;
-                        while(ptr->next!=NULL)
-                        {
-                            ptr=ptr->next;
-                        }
-                        node->next=ptr->next;
-                        ptr->next=node;
-                    case 3:
-                        printf("\nEnter the position to insert at: ");
-                        scanf("%d",&pos);
-                        ptr=head;
-                        
-                }
+                push(size,&top);
+                break;
+            case 2:
+                pop(&top);
+                break;
+            case 3:
+                peek(top);
+                break;
+            case 4:
+                display(top);
+                break;
+            case 5:
+                if(top==size-1)
+                {printf("\nStack is full");}
+                else
+                {printf("\nStack is not full");}
+                break;
+            case 6:
+                if(top==-1)
+                {printf("\nStack is empty");}
+                else
+                {printf("\nStack is not empty");}
+                break;
+            case 7:
+                exit(0);
+            default:
+                printf("\nInvalid choice. Please check again");
         }
     }
+    return 0;
 }
